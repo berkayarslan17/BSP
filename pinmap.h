9a488933c79fa5e0587ac85a95e5844731000c91
Berkay Arslan <44584158+berkayarslan17@users.noreply.github.com>
2021-03-14 01:42:53 +0300

    Update pinmap.h

--- pinmap.h
+++ pinmap.h
@@ -8,6 +8,17 @@ typedef struct PIN_STRUCT{
     char const type;
     char const* state;
     int num;
+    int adc_channel;
 } PIN;
+
+typedef struct TIM_STRUCT{
+    int num;
+    int TRGx;
+    int APBENR_type;
+    int APBENR_num;
+    int priority;
+    int PSC;
+    int ARR;
+} TIM;
 
 #endif /* PINMAP_H_ */
