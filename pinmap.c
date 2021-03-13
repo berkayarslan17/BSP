537fbe6e7527129e916cf05762276465ddab9551
Berkay Arslan <44584158+berkayarslan17@users.noreply.github.com>
2021-03-14 01:42:29 +0300

    Update pinmap.c

--- pinmap.c
+++ pinmap.c
@@ -2,134 +2,235 @@
 #include "pinmap.h"
 
 PIN PA0 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 0,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 0,
+    .adc_channel = 0,
 };
 
 PIN PA1 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 1,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 1,
+    .adc_channel = 1,
 };
 
 PIN PA4 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 4,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 4,
+    .adc_channel = 4,
 };
 
 PIN PA5 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 5,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 5,
+    .adc_channel = 5,
 };
 
 PIN PA6 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 6,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 6,
+    .adc_channel = 6,
 };
 
 PIN PA7 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 7,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 7,
+    .adc_channel = 7,
 };
 
 PIN PA8 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 8,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 8,
 };
 
 PIN PA9 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 9,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 9,
 };
 
 PIN PA10 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 10,
-};
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 10,
+};     
 
 PIN PA11 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 11,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 11,
+    .adc_channel = 15,
 };
 
 PIN PA12 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 12,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 12,
+    .adc_channel = 16,
 };
 
 PIN PA15 {
-    .type  = 'A',
-    .state = "OUTPUT",
-    .num   = 15,
+    .type        = 'A',
+    .state       = "OUTPUT",
+    .num         = 15,
 };
 
 PIN PB0 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 0,
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 0,
+    .adc_channel = 8,
 };
 
 PIN PB1 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 1,
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 1,
+    .adc_channel = 9,
 };
 
 PIN PB2 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 2,
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 2,
+    .adc_channel = 10,
 };
 
 PIN PB3 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 3,
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 3,
 };
 
 PIN PB4 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 4,
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 4,
+};      
+      
+PIN PB5 {      
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 5,
+};      
+      
+PIN PB6 {      
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 6,
+};      
+      
+PIN PB7 {      
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 7,
+};      
+      
+PIN PB8 {      
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 8,
+};      
+      
+PIN PB9 {      
+    .type        = 'B',
+    .state       = "OUTPUT",
+    .num         = 9,
 };
 
-PIN PB5 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 5,
+TIM TIM1 {
+    .num  = 1,
+    .TRGx = 1,
+    .APBENR_type = 2,
+    .APBENR_num = 11,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
 };
 
-PIN PB6 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 6,
+TIM TIM2 {
+    .num  = 2,
+    .TRGx = 2,
+    .APBENR_type = 1,
+    .APBENR_num = 0,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
 };
 
-PIN PB7 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 7,
+TIM TIM3 {
+    .num  = 3,
+    .TRGx = 3,
+    .APBENR_type = 1,
+    .APBENR_num = 1,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
 };
 
-PIN PB8 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 8,
+TIM TIM6 {
+    .num  = 6,
+    .TRGx = 6,
+    .APBENR_type = 1,
+    .APBENR_num = 4,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
 };
 
-PIN PB9 {
-    .type  = 'B',
-    .state = "OUTPUT",
-    .num   = 9,
+TIM TIM7 {
+    .num  = 7,
+    .TRGx = 7,
+    .APBENR_type = 1,
+    .APBENR_num = 5,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
+};
+
+TIM TIM14 {
+    .num  = 14,
+    .TRGx = 0,
+    .APBENR_type = 2,
+    .APBENR_num = 15,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
+};
+
+TIM TIM15 {
+    .num  = 15,
+    .TRGx = 0,
+    .APBENR_type = 2,
+    .APBENR_num = 16,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
+};
+
+TIM TIM16 {
+    .num  = 16,
+    .TRGx = 0,
+    .APBENR_type = 2,
+    .APBENR_num = 17,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
+};
+
+TIM TIM17 {
+    .num  = 17,
+    .TRGx = 0,
+    .APBENR_type = 2,
+    .APBENR_num = 18,
+    .priority = 0,
+    .PSC = 0,
+    .ARR = 0,
 };
 
